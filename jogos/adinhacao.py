8c1e85d7469750a7837c762fe985a09dd5737796
degovercosa <guilherme.vercosa@fatec.sp.gov.br>
2021-08-23 10:00:43 -0300

    implementação do jogo de adivinhação a partir de variáveis, do comando if-else e do comando input

--- jogos/adinhacao.py
+++ jogos/adinhacao.py
@@ -1,1 +1,14 @@
+print("#################################")
 print("Bem vindo ao jogo de advinhação !")
+print("#################################")
+
+numero_secreto = 76
+
+chute = int(input("Digite o seu número: "))
+
+print("você digitou: ", chute)
+
+if (numero_secreto == chute):
+    print("você acertou")
+else:
+    print("você errou")
