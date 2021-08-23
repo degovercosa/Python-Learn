9b1e4682a097873c2c0825a256b2c03858d9498a
degovercosa <guilherme.vercosa@fatec.sp.gov.br>
2021-08-23 19:35:24 -0300

    correção de erros: 1- a limitação estava no número chutado e não nas tentativas

--- jogos/adinhacao.py
+++ jogos/adinhacao.py
@@ -3,17 +3,22 @@ print("Bem vindo ao jogo de advinhação !")
 print("#################################")
 
 numero_secreto = 76
-total_de_tentativas = int(input("Insira o número de tentativas: "))
+total_de_tentativas = int(input("Insira o número de tentativas de 1 a 20: "))
+
 
 for rodada in range (0, total_de_tentativas):
-    print("Rodada {} de {} tentativas".format(rodada, total_de_tentativas))
-    chute = int(input("Digite o seu número: "))
+    print("Rodada {} de {} tentativas".format(rodada + 1, total_de_tentativas))
+    if (total_de_tentativas > 20 or total_de_tentativas < 1):
+        print("Você digitou número fora do escopo")
+        break
+    chute = int(input("Digite um número: "))
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     print("você digitou: ", chute)
 
     if (acertou):
         print("você acertou")
+        break
     elif (maior):
         print("você errou, seu chute foi maior que o número secreto")
     else:
