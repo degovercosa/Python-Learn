038b105edd39b48347a70da76ee596b2149a29c5
degovercosa <guilherme.vercosa@fatec.sp.gov.br>
2021-08-23 10:35:11 -0300

    implementação de um looping para a adição de mais tentativas

--- jogos/adinhacao.py
+++ jogos/adinhacao.py
@@ -4,11 +4,27 @@ print("#################################")
 
 numero_secreto = 76
 
-chute = int(input("Digite o seu número: "))
+tentativas = int(input("Insira o número de tentativas: "))
 
-print("você digitou: ", chute)
+while (tentativas > 0):
 
-if (numero_secreto == chute):
-    print("você acertou")
-else:
-    print("você errou")
+    chute = int(input("Digite o seu número: "))
+
+    acertou = chute == numero_secreto
+
+    maior = chute > numero_secreto
+
+    print("você digitou: ", chute)
+
+    if (acertou):
+        print("você acertou")
+    elif (maior):
+        print("você errou, seu chute foi maior que o número secreto")
+    else:
+        print("você errou, seu chute foi menor que o número secreto")
+    tentativas -= 1
+    print("O número de tentativas restantes é: ", tentativas)
+
+print("############")
+print("Fim de jogo!")
+print("############")
