4338439d6d892004d7b0b1f4760f503a220af8fa
degovercosa <guilherme.vercosa@fatec.sp.gov.br>
2021-08-24 10:40:42 -0300

    sistema de pontuação

--- jogos/adinhacao.py
+++ jogos/adinhacao.py
@@ -3,8 +3,9 @@ import random
 print("#################################")
 print("Bem vindo ao jogo de advinhação !")
 print("#################################")
-# definição do número a ser encontrado
+# definição do número a ser encontrado e da pontuação
 numero_secreto = random.randint(1, 100)
+pontos = 1000
 
 # definição do nível de dificuldade
 print("Insira o nível de dificuldade")
@@ -24,6 +25,7 @@ while (total_de_tentativas < 5):
 # definição do corpo do jogo
 for rodada in range (0, total_de_tentativas):
     print(f"Rodada {rodada + 1} de {total_de_tentativas} tentativas")
+    print(f"Sua pontuação é {pontos}")
     chute = int(input("Digite um número de 1 a 100: "))
     if(chute < 1 or chute > 100):
         print("Você chutou um número fora do escopo")
@@ -32,13 +34,16 @@ for rodada in range (0, total_de_tentativas):
     maior = chute > numero_secreto
     print("você digitou: ", chute)
     if (acertou):
-        print("você acertou")
+        print(f"você acertou, pontos: {pontos}")
         break
-    elif (maior):
-        print("você errou, seu chute foi maior que o número secreto")
     else:
-        print("você errou, seu chute foi menor que o número secreto")
-
+        if(maior):
+            print("você errou, seu chute foi maior que o número secreto")
+        else:
+            print("você errou, seu chute foi menor que o número secreto")
+        pontos = pontos - (abs(numero_secreto - chute))
+    if (rodada + 1 == total_de_tentativas):
+        print(f"O número era {numero_secreto}")
 print("############")
 print("Fim de jogo!")
 print("############")
