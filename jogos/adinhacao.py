1acae2d3ec96bbba06649568c0ebf05e9bbfaacb
degovercosa <guilherme.vercosa@fatec.sp.gov.br>
2021-08-24 09:34:24 -0300

    limitação no número chutado e inserção do módulo random

--- jogos/adinhacao.py
+++ jogos/adinhacao.py
@@ -1,21 +1,25 @@
+import random
+
 print("#################################")
 print("Bem vindo ao jogo de advinhação !")
 print("#################################")
 
-numero_secreto = 76
+numero_secreto = random.randint(1, 100)
+
 total_de_tentativas = int(input("Insira o número de tentativas de 1 a 20: "))
 
-
 for rodada in range (0, total_de_tentativas):
-    print("Rodada {} de {} tentativas".format(rodada + 1, total_de_tentativas))
+    print(f"Rodada {rodada + 1} de {total_de_tentativas} tentativas")
     if (total_de_tentativas > 20 or total_de_tentativas < 1):
-        print("Você digitou número fora do escopo")
+        print("Você digitou número de tentativas fora do escopo")
         break
-    chute = int(input("Digite um número: "))
+    chute = int(input("Digite um número de 1 a 100: "))
+    if(chute < 1 or chute > 100):
+        print("Você chutou um número fora do escopo")
+        continue
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     print("você digitou: ", chute)
-
     if (acertou):
         print("você acertou")
         break
